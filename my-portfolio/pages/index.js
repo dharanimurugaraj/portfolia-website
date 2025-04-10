26b3a0a21106ddec01c5305a2e399c3d81b365b2
Dharani M <113276061+dharanimurugaraj@users.noreply.github.com>
2025-04-10 09:58:19 +0530

    temp

---
+++ my-portfolio/pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+      <div className="min-h-screen bg-gray-900 text-white">
+        <h1 className="text-4xl font-bold text-cyan-400 p-4">Hello, Tailwind v4!</h1>
+      </div>
+    );
+  }
